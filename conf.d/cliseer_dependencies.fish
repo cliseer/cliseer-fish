b699911ffab57b2a016066fa0354c4b46480d5ba
Yaron Greif <ygreif@gmail.com>
2025-05-12 16:50:39 -0700

    Download from github workflow instead of pip

--- conf.d/cliseer_dependencies.fish
+++ conf.d/cliseer_dependencies.fish
@@ -10,22 +10,59 @@ function maybe_run
 end
 
 function _cliseer_cliprophesy_dependency --on-event cliseer_dependencies_install
-     # Check if cliprophesy is installed as a Python module
-     if type -q cliprophesy
-        echo "cliprophesy is installed"
+    # Check if cliprophesy is already installed in PATH
+    if type -q cliprophesy
+        echo "cliprophesy is already installed."
         return 0
-     end
-     
-     echo "cliprophesy, CLI helper module, is required"
-     maybe_run "pip install cliprophesy --user"
-     
-     if test $status -ne 0
-        echo "Attempting to install cliprophesy system-wide..."
-        maybe_run "pip install cliprophesy"
-     end
+    end
 
-     set -Ux __cliseer_bin_dir (python3 -m site --user-base)/bin
-     return 0
+    echo "cliprophesy, a CLI helper module, is required."
+
+    # Set platform-specific binary name
+    switch (uname)
+        case Linux
+            set binary_name cliprophesy-linux
+        case Darwin
+            set binary_name cliprophesy-macos
+        case '*'
+            echo "Unsupported OS: (uname)"
+            return 1
+    end
+
+    read -P "Download release from github? [y/N] " -l answer 
+    if test "$answer" != "y"
+       echo "Installation skipped, for manual installations see https://github.com/cliseer/cliprophesy"
+       return 0
+    end
+
+    # Set install location
+    set bin_dir $HOME/.local/bin
+    mkdir -p $bin_dir
+
+    # Download latest release
+    set url "https://github.com/cliseer/cliprophesy/releases/latest/download/$binary_name"
+    set dest "$bin_dir/cliprophesy"
+    echo "Downloading $binary_name from $url to $dest"
+    curl -L $url -o $dest
+
+    # Make executable
+    chmod +x $dest
+
+    # Check if bin_dir is in PATH
+    if not contains $bin_dir $PATH
+        echo ""
+        echo "Directory $bin_dir is not in your PATH."
+        read -P "Add $bin_dir to your PATH? [y/N] " -l answer
+        if test "$answer" = "y"
+            set -Ux fish_user_paths $bin_dir $fish_user_paths
+            echo "Added $bin_dir to your PATH."
+        else
+            echo "You can add it later with:"
+            echo "  set -Ux fish_user_paths $bin_dir \$fish_user_paths"
+        end
+    end
+
+    return 0
 end
 
 function _config_dependency --on-event cliseer_dependencies_install
